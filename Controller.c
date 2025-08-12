38dd48c892de10e22dfef858e98454672fabd6e2
KeepItSimple <133901572+KeepltSimple@users.noreply.github.com>
2025-08-12 20:30:27 +0100

    Add function to generate key for shared memory
    Creates a temporary file and uses it to generate a key via ftok().
    Ensures the code works on a fresh system by avoiding failures from a
    missing /tmp file.

--- Controller.c
+++ Controller.c
@@ -1,8 +1,46 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
+#include <sys/ipc.h>
+
+typedef struct transaction
+{
+    char id[20];
+    unsigned int reward;
+    unsigned int value;
+    time_t timeStamp;
+} transaction;
+
+typedef struct transactionPendingSet
+{
+    int empty;
+    int age;
+    transaction currTransaction;
+} transactionPendingSet;
+
+key_t createKey(char);
 
 int main()
 {
 
+    key_t key = createKey('A');
+    if (key == 1)
+    {
+        return 0;
+    }
+
     return 0;
+}
+
+key_t createKey(char projId)
+{
+    const char *path = "/tmp/myproject.ipc";
+    int fd = open(path, O_CREAT | O_RDWR, 0666);
+    if (fd == -1)
+    {
+        perror("open");
+        return 1;
+    }
+
+    return ftok(path, projId);
 }

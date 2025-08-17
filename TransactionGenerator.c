488db344159adddcefe5ca3e4f01dfb138d90173
KeepItSimple <133901572+KeepltSimple@users.noreply.github.com>
2025-08-17 10:20:49 +0100

    Add slight refactoring

--- TransactionGenerator.c
+++ TransactionGenerator.c
@@ -13,13 +13,27 @@
 
 int isCMDValid(int, unsigned int *, unsigned int *, char **);
 void generateTransaction(int, pid_t, transaction *);
-void sendTransaction(transaction *, int);
+void sendTransaction(transaction *, int, transactionPendingSet *);
 
 int main(int argc, char **argv)
 {
 
-    sem_t *poolSem = NULL;
+    pid_t pid;
     int poolLength = 0;
+    unsigned int reward = 0;
+    unsigned int timeIntervalMs = 0;
+    transaction newTransaction;
+    transactionPendingSet *pendingTransactions = NULL;
+    key_t poolKey = 0;
+    sem_t *poolSem = NULL;
+
+    poolKey = createPoolKey();
+    if (poolKey == -1)
+    {
+        perror("ftok");
+        exit(1);
+    }
+
     poolSem = sem_open(POOL_SEMA, 0);
 
     if (poolSem == SEM_FAILED)
@@ -28,9 +42,6 @@ int main(int argc, char **argv)
         exit(1);
     }
 
-    unsigned int reward = 0;
-    unsigned int timeIntervalMs = 0;
-
     if (!isCMDValid(argc, &reward, &timeIntervalMs, argv))
     {
         return 0;
@@ -38,22 +49,9 @@ int main(int argc, char **argv)
 
     srand(time(NULL));
 
-    transaction newTransaction;
-    transactionPendingSet *pendingTransactions = NULL;
-
-    key_t poolKey = createPoolKey();
-
-    if (poolKey == -1)
-    {
-        perror("ftok");
-        exit(1);
-    }
-
     sem_wait(poolSem);
     atachToTrnsPool(&pendingTransactions, poolKey);
     poolLength = getPoolSize(poolKey) / sizeof(transactionPendingSet);
-
-    pid_t pid;
 
     while (1)
     {
@@ -62,18 +60,25 @@ int main(int argc, char **argv)
         if (pid == 0)
         {
             generateTransaction(reward, getpid(), &newTransaction);
-            printf("ID:%s\nReward:%d\nValue:%d\nTimeStamp:%ld\n", newTransaction.id, newTransaction.reward, newTransaction.value, newTransaction.timeStamp);
-            // send transaction
+            // printf("ID:%s\nReward:%d\nValue:%d\nTimeStamp:%ld\n", newTransaction.id, newTransaction.reward, newTransaction.value, newTransaction.timeStamp);
+            sendTransaction(&newTransaction, poolLength, pendingTransactions);
 
             exit(0);
         }
         else
         {
+
             usleep(timeIntervalMs * 1000);
             wait(NULL);
         }
     }
 
+    if (shmdt(pendingTransactions) == -1)
+    {
+        perror("shmdt");
+    }
+
+    sem_post(poolSem);
     sem_close(poolSem);
 
     return 0;
@@ -115,4 +120,21 @@ void generateTransaction(int reward, pid_t pid, transaction *newTransaction)
     newTransaction->timeStamp = time(NULL);
     newTransaction->value = rand() % 2000 + 1;
     snprintf(newTransaction->id, sizeof(newTransaction->id), "%d-%ld", pid, newTransaction->timeStamp);
+}
+
+void sendTransaction(transaction *newTransaction, int poolLimit, transactionPendingSet *pendingTransactions)
+{
+
+    for (int i = 0; i < poolLimit; i++)
+    {
+        if (!pendingTransactions[i].empty)
+        {
+            strcpy(pendingTransactions[i].currTransaction.id, newTransaction->id);
+            pendingTransactions[i].currTransaction.reward = newTransaction->reward;
+            pendingTransactions[i].currTransaction.timeStamp = newTransaction->timeStamp;
+            pendingTransactions[i].currTransaction.value = newTransaction->value;
+            pendingTransactions[i].empty = 1;
+            break;
+        }
+    }
 }
